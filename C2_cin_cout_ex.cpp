0df3e2f5038aca1f1d96b330bfebec6a8c616029
Don Jacob <donjacob10@gmail.com>
2017-04-23 00:22:09 -0700

    Intro-Still-Refreshing1

--- C2_cin_cout_ex.cpp
+++ C2_cin_cout_ex.cpp
@@ -9,6 +9,7 @@ int main() {
 	<< direction of r-value. from r-value to console
 	*/
 	int x;
+	std::cout << "[Typing too big a value will cause overflow since integer variable can hold only so much. Try it !!]" << std::endl;
 	std::cout << "Enter a number: ";
 	std::cin >> x;
 	std::cout << "Variable entered is " << x << endl;
